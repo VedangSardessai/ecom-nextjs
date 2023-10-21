d26d7081b2240b0cb430f00b1114f47bf88b6062
VedangSardessai <vms112000@gmail.com>
2023-10-21 14:33:11 +0530

    code : dbconfig and usermodel created

---
+++ src/dbconfig/dbconfig.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URL!)
+
+        const connection = mongoose.connection
+
+        connection.on(
+            'connected', () => console.log('Connected successfully')
+
+        )
+
+        connection.on(
+            'error', (err) => console.log('Connection error', err)
+
+        )
+    }
+
+    catch (error) {
+        console.log('error caught', error);
+
+    }
+}
