616e0f6c608f0583907fbfce8a0bac428e2aa52e
VedangSardessai <vms112000@gmail.com>
2023-10-27 21:23:36 +0530

    code : helper for getting userData

---
+++ src/helpers/getUserDataFromToken.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server"
+import jwt, { JwtPayload } from "jsonwebtoken"
+
+
+export const getUserData = (request: NextRequest) => {
+    try {
+        const token = request.cookies.get('token')?.value || ''
+
+        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!)
+
+        return decodedToken.id
+    } catch (error) {
+
+    }
+}
