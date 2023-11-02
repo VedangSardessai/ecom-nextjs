673d4b25d24c171e042d1e80453651e45401131c
VedangSardessai <vms112000@gmail.com>
2023-11-03 01:52:55 +0530

    bug: check if products is defined

--- src/app/products/productcomponent/page.tsx
+++ src/app/products/productcomponent/page.tsx
@@ -5,7 +5,7 @@ export default function DisplayProducts({ products }: any) {
   const router = useRouter();
   return (
     <div className="m-5 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
-      {products.map((product: any) => (
+      {products?.map((product: any) => (
         <div
           onClick={() =>
             router.push("/products/buynow/" + product.id.toString())
