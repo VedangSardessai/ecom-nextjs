fdb9edebcb58a9617fb85d7e4792aef48a781d35
VedangSardessai <vms112000@gmail.com>
2023-11-03 00:05:30 +0530

    ui: displays individual product

---
+++ src/app/products/buynow/[id]/page.tsx
@@ -0,0 +1,281 @@
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function BuyNow(): JSX.Element {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [productFromId, setProductFromId] = useState<any>([]);
+  const [images, setImages] = useState<any>([]);
+  const [desc, setDesc] = useState<any>("");
+  const getProductFromId = async () => {
+    const product = pathname.split("/buynow/")[1];
+    const apiurl = "https://dummyjson.com/products/" + product;
+
+    const response = await axios.get(apiurl);
+    console.log(response.data);
+
+    setProductFromId(response.data);
+    setImages(response.data.images);
+    setDesc(response.data.description);
+  };
+  const [selectedSize, setSelectedSize] = useState<string | null>(null);
+
+  // Function to handle size button click
+  const handleSizeClick = (size: string) => {
+    setSelectedSize(size);
+  };
+  useEffect(() => {
+    getProductFromId();
+  }, []);
+
+  return (
+    <div className="bg-white">
+      <div className="pt-6">
+        <nav aria-label="Breadcrumb">
+          <ol
+            role="list"
+            className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
+          >
+            <li className="text-sm">
+              <p
+                aria-current="page"
+                className="font-medium text-gray-500 hover:text-gray-600 capitalize"
+              >
+                {productFromId.title}
+              </p>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+          <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+            <img
+              src={images[3]}
+              alt="Two each of gray, white, and black shirts laying flat."
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+              <img
+                src={images[3]}
+                alt="Model wearing plain black basic tee."
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+              <img
+                src={images[3]}
+                alt="Model wearing plain gray basic tee."
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+          </div>
+          <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+            <img
+              src={images[3]}
+              alt="Model wearing plain white basic tee."
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+        </div>
+
+        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className=" capitalize text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              {productFromId.title}
+            </h1>
+          </div>
+
+          <div className="mt-4 lg:row-span-3 lg:mt-0">
+            <h2 className="sr-only">Product information</h2>
+            <p className="text-3xl tracking-tight text-gray-900">
+              {" "}
+              ₹{75 * productFromId.price}
+            </p>
+
+            <div className="mt-6">
+              <h3 className="sr-only">Reviews</h3>
+              <div className="flex items-center">
+                <div className="flex items-center">
+                  <svg
+                    className="text-gray-900 h-5 w-5 flex-shrink-0"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    className="text-gray-900 h-5 w-5 flex-shrink-0"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    className="text-gray-900 h-5 w-5 flex-shrink-0"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    className="text-gray-900 h-5 w-5 flex-shrink-0"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    className="text-gray-200 h-5 w-5 flex-shrink-0"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <p className="sr-only">4 out of 5 stars</p>
+                <p className="cursor-pointer ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                  117 reviews
+                </p>
+              </div>
+            </div>
+
+            <form className="mt-10">
+              <div className="mt-10">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm font-medium text-gray-900">Size</h3>
+                </div>
+
+                <fieldset className="mt-4">
+                  <legend className="sr-only">Choose a size</legend>
+                  <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                    <label className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-not-allowed bg-gray-50 text-gray-400">
+                      <input
+                        type="radio"
+                        name="size-choice"
+                        value="XXS"
+                        disabled
+                        className="sr-only"
+                        aria-labelledby="size-choice-0-label"
+                      />
+                      <span id="size-choice-0-label">XXS</span>
+                      <span
+                        aria-hidden="true"
+                        className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
+                      >
+                        <svg
+                          className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                          viewBox="0 0 100 100"
+                          preserveAspectRatio="none"
+                          stroke="currentColor"
+                        >
+                          <line
+                            x1="0"
+                            y1="100"
+                            x2="100"
+                            y2="0"
+                            vectorEffect="non-scaling-stroke"
+                          />
+                        </svg>
+                      </span>
+                    </label>
+                    {["XS", "S", "M", "L", "XL", "2XL", "3XL"].map((size) => (
+                      <label
+                        key={size}
+                        className={`group relative flex items-center justify-center rounded-md py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 ${
+                          selectedSize === size
+                            ? "border-indigo-600 border-4 " // Change border color for selected size
+                            : "border-gray-200 border"
+                        } cursor-pointer bg-white text-gray-900 shadow-sm`}
+                        onClick={() => handleSizeClick(size)}
+                      >
+                        <input
+                          type="radio"
+                          name="size-choice"
+                          value={size}
+                          className="sr-only"
+                          aria-labelledby={`size-choice-${size}-label`}
+                        />
+                        <span id={`size-choice-${size}-label`}>{size}</span>
+
+                        <span
+                          className="pointer-events-none absolute -inset-px rounded-md"
+                          aria-hidden="true"
+                        ></span>
+                      </label>
+                    ))}
+                  </div>
+                </fieldset>
+              </div>
+
+              <button
+                type="submit"
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Add to bag
+              </button>
+            </form>
+          </div>
+
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+            <div>
+              <h3 className="sr-only">Description</h3>
+
+              <div className="space-y-6">
+                <p className="te capitalize xt-base text-gray-900">
+                  The Basic {productFromId.title}
+                  you to fully express your vibrant personality. Feeling
+                  adventurous? or Want to be a trendsetter? Try out our{" "}
+                  {productFromId.title}.
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+              <div className="mt-4">
+                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                  {desc.split(",").map((part: any, index: any) => (
+                    <li key={index} className="text-gray-400">
+                      <span className="text-gray-600">{part.trim()}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
