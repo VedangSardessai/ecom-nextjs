2de915f1dc8a1fa643d973684e7988f2409b31eb
VedangSardessai <vms112000@gmail.com>
2023-11-03 21:16:44 +0530

    code: setup of redux in app done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavigationComponent from "./navigation/page";
+import { ReduxProvider } from "@/redux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavigationComponent />
-        {children}
+        <ReduxProvider>
+          <NavigationComponent />
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
