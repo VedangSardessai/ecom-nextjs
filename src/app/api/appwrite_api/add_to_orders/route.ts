3afc30881e7b4d601eb219aceac8068a1d6bb57f
VedangSardessai <vms112000@gmail.com>
2023-11-14 00:58:34 +0530

    code: commented logs

--- src/app/api/appwrite_api/add_to_orders/route.ts
+++ src/app/api/appwrite_api/add_to_orders/route.ts
@@ -10,26 +10,24 @@ connectAppwrite()
 
 export async function POST(request: NextRequest) {
 
-    console.log('trying POST request');
+    // console.log('trying POST request');
 
     const id = await ID.unique()
-    console.log(id);
 
 
     try {
         const reqBody = await request.json();
-        console.log(reqBody);
-
+        // console.log(reqBody);
         // console.log(formData)
         const response = await databases.createDocument(conf.db, conf.collection, id, reqBody);
 
-        console.log(response); // Success
+        // console.log(response); // Success
         return NextResponse.json({
             message: response.message,
             success: true,
         });
     } catch (error: any) {
-        console.log(error.message);
+        // console.log(error.message);
 
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
