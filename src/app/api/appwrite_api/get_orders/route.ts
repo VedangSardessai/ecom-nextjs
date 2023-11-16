42b080bdfd76d28c128a56895216d95e2b71b346
VedangSardessai <vms112000@gmail.com>
2023-11-16 21:11:50 +0530

    code: check if user_id exists then run other func

--- src/app/api/appwrite_api/get_orders/route.ts
+++ src/app/api/appwrite_api/get_orders/route.ts
@@ -10,16 +10,20 @@ connectAppwrite()
 export async function GET(request: NextRequest) {
     try {
         const user_id = await getUserData(request)
-        const response = await databases.listDocuments(conf.db, conf.collection, [Query.equal('user_id', user_id)])
+        if (user_id) {
+            const response = await databases.listDocuments(conf.db, conf.collection, [Query.equal('user_id', user_id)])
 
-        console.log(user_id, 'user_id');
-        console.log(response);
-        
+            console.log(user_id, 'user_id');
+            console.log(response.documents);
 
 
-        return NextResponse.json({
-            response: response
-        });
+
+            return NextResponse.json({
+                response: response
+            });
+        }
+        throw new Error("Eror geing user ID");
+
     } catch (error: any) {
         console.error(error);
 
